49ddf236a2996b90136cd2535356837ec824a645
Mike He <hequan@qianchengtec.com>
2019-03-14 17:39:06 +0800

    change wx

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $(function(){
 })
 
 // -------------分享-------------
-getShare()
+// getShare()
 
 function getShare(){
    $.ajax({
@@ -69,6 +69,41 @@ function getShare(){
         });
      }
    })
+}
+
+getQrCode()
+
+function getQrCode() {
+    $.ajax({
+        url: 'http://mm.diandianboke.com:8000/wechat/share/signature',
+        data: {
+            "app_id": "wxba7f7c2f25ed9cc8",
+            "url": encodeURIComponent(window.location.href.split('#')[0])
+        },
+        type: 'POST',
+        dataType: 'json',
+        success: function(resp){
+            wx.config({
+                // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: resp.appId, // 必填，公众号的唯一标识
+                timestamp: resp.timestamp, // 必填，生成签名的时间戳
+                nonceStr: resp.nonceStr, // 必填，生成签名的随机串
+                signature: resp.signature,// 必填，签名
+                jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表
+            });
+
+            wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+                wx.scanQRCode({
+                    needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                    scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                    success: function (res) {
+                        var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                        alert(result);
+                    }
+                });
+            });
+        }
+    })
 }
 
 var isClick = true;
